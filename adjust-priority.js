7eba9ddecb7fc797fb1d61564dddd9455e174138
Ian Campos <iancampos.0@gmail.com>
2020-10-23 12:23:32 -0700

    replace adjust_priority with increase_priority

--- adjust-priority.js
+++
@@ -1,6 +0,0 @@
-function adjust_priority(task) {
-    if (task) {
-        task.set("Priority", task.field("Priority") + arg("priority delta"));
-        adjust_priority(get_parent(task));
-    }
-}

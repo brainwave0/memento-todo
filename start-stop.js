d10040712d5d8a9554a9e1a109907b14054432cb
Ian Campos <iancampos.0@gmail.com>
2020-10-23 13:07:44 -0700

    switch away from getters and setters

--- start-stop.js
+++ start-stop.js
@@ -1,20 +1,20 @@
 function start(task) {
     setTimer();
-    setTimerStart(task, Date.now());
+    task.set("Timer start", Date.now());
 }
 function finish(task) {
-    setRuntime(task, runtime() + elapsed());
+    task.set("Runtime", entry.field("Runtime") + elapsed());
     updateWaitTimes(elapsed());
     task.set("Wait time", 0);
 }
 function setTimer() {
-    AndroidAlarm.timer(Math.min(timeSlice() / 1000, 120), name(), false);
+    AndroidAlarm.timer(Math.min(timeSlice() / 1000, 120), entry().field("Name"), false);
 }
 function timeSlice() {
     return entry().field("Wait time") / entryAndSiblings().length;
 }
 function toggleRunning(task) {
-    setRunning(task, !running(task));
+    task.set("Running", !task.field("Running"));
 }
 function main(task) {
     if (running(task)) {
@@ -26,32 +26,8 @@ function main(task) {
     var parent = get_parent(task);
     if (parent) {main(parent);}
 }
-function setRunning(task, newValue) {
-    task.set("Running", newValue);
-}
-function running(task) {
-    return task.field("Running");
-}
-function setTimerStart(task, newValue) {
-    task.set("Repeat From", newValue);
-}
-function setRuntime(task, newValue) {
-    task.set("Runtime", newValue);
-}
-function timerStart() {
-    return entry().field("Repeat From");
-}
 function elapsed() {
     return Date.now() - timerStart();
-}
-function setPrevTime(task, newValue) {
-    task.set("Previous Time", newValue);
-}
-function name() {
-    return entry().field("Name");
-}
-function runtime() {
-    return entry().field("Runtime");
 }
 function entryAndSiblings() {
     return get_parent(entry()).field("Subtasks");

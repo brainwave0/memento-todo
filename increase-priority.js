6563c94b76e5079e002b02cf051eba2692a54e28
Ian Campos <iancampos.0@gmail.com>
2020-10-24 10:09:38 -0700

    convert increase-priority.js to entry action

--- increase-priority.js
+++ increase-priority.js
@@ -2,8 +2,4 @@ function increase_priority(task) {
     task.set("Priority", task.field("Priority") + 1);
 }
 
-var entries = selectedEntries();
-for (var i = 0; i < entries.length; i++) {
-    var entry = entries[i];
-    increase_priority(entry);
-}
+increase_priority(entry());

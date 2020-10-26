9bd6f2522ca2e013c8e2543d25f9c504a1cff79d
Ian Campos <iancampos.0@gmail.com>
2020-10-26 11:56:37 -0700

    fix: reschedule with repeat interval did nothing

--- reschedule.js
+++ reschedule.js
@@ -1,7 +1,7 @@
 if (arg("Specific datetime")) {
     entry().set("Start datetime", arg("Specific datetime"));
 } else if (arg("Repeat interval")) {
-    entry().set("Start datetime", Date.now() + arg("Repeat interval"));
+    entry().set("Start datetime", Date.now() + entry().field("Repeat interval"));
 } else if (arg("Specific duration")) {
     entry().set("Start datetime", Date.now() + arg("Specific duration"));
 }

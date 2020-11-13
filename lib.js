b41dea6d07f98ab9ebf288282fa12f24ac310e87
Ian Campos <iancampos.0@gmail.com>
2020-11-13 13:04:18 -0800

    remove hierarchial tasks

--- lib.js
+++ lib.js
@@ -1,11 +1,3 @@
-function get_parent(task) {
-    var parents = lib().linksTo(task);
-    if (parents.length > 0) {
-        return parents[0];
-    } else {
-        return undefined;
-    }
-}
 function to_array(iterable) {
     var result = [];
     for (var i = 0; i < iterable.length; i++) {
--- start-stop.js
+++ start-stop.js
@@ -1,23 +1,19 @@
 function start(task) {
-    if (task == entry()) {
-        setTimer();
-        create_log_entry('Started task "' + entry().field("Name") + '"');
-    }
+    setTimer();
+    create_log_entry('Started task "' + entry().field("Name") + '"');
     task.set("Timer start", Date.now());
 }
 function finish(task) {
     task.set("Runtime", task.field("Runtime") + elapsed());
-    if (task == entry()) {
-        updateWaitTimes(elapsed());
-        create_log_entry('Finished task "' + entry().field("Name") + '"');
-    }
+    updateWaitTimes(elapsed());
+    create_log_entry('Finished task "' + entry().field("Name") + '"');
     task.set("Wait time", 0);
 }
 function setTimer() {
     AndroidAlarm.timer(Math.max(timeSlice() / 1000, 16 * 60), entry().field("Name"), false);
 }
 function timeSlice() {
-    return entry().field("Wait time") / leafTasks().length;
+    return entry().field("Wait time") / lib().entries().length;
 }
 function toggleRunning(task) {
     task.set("Running", !task.field("Running"));
@@ -29,14 +25,9 @@ function main(task) {
         start(task);
     }
     toggleRunning(task);
-    var parent = get_parent(task);
-    if (parent) { main(parent); }
 }
 function elapsed() {
     return Date.now() - entry().field("Timer start");
-}
-function leafTasks() {
-    return to_array(lib().entries()).filter(x => x.field("Subtasks").length == 0);
 }
 function updateWaitTimes(duration) {
     var entries = lib().entries();

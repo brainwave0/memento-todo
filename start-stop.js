fc6afa8617b62dea7504916ca0e364e649c913bf
Ian Campos <iancampos.0@gmail.com>
2020-11-13 12:39:57 -0800

    Revert "new system: difficulty hierarchy"
    This reverts commit e36022b20404b87650576fcd090aa8c2da93a166.

--- start-stop.js
+++ start-stop.js
@@ -1,40 +1,36 @@
 function start(task) {
-    if (!task.field("Running")) {
-        toggleRunning(task);
-        create_log_entry('Started task "' + task.field("Name") + '"');
-        task.set("Timer start", Date.now());
+    if (task == entry()) {
+        setTimer();
+        create_log_entry('Started task "' + entry().field("Name") + '"');
     }
-    if (is_root(task)) {
-        start_timers(task);
-    } else {
-        start(get_parent(task));
-    }
+    task.set("Timer start", Date.now());
 }
 function finish(task) {
     task.set("Runtime", task.field("Runtime") + elapsed());
-    if (is_root(task)) { updateWaitTimes(elapsed()); }
-    create_log_entry('Finished task "' + entry().field("Name") + '"');
+    if (task == entry()) {
+        updateWaitTimes(elapsed());
+        create_log_entry('Finished task "' + entry().field("Name") + '"');
+    }
     task.set("Wait time", 0);
-    if (running_child(task)) { finish(running_child(task)) }
-    toggleRunning(task);
-    task.set("timer running", false);
 }
-function setTimer(task, duration) {
-    task.set("timer running", true);
-    AndroidAlarm.timer(duration, task.field("Name"), false);
+function setTimer() {
+    AndroidAlarm.timer(Math.max(timeSlice() / 1000, 16 * 60), entry().field("Name"), false);
 }
-function time_slice(duration, tasks, task) {
-    return task.field("Priority") / tasks.map(x => x.field("Priority")).reduce((a, b) => a + b, 0) * duration;
+function timeSlice() {
+    return entry().field("Wait time") / leafTasks().length;
 }
 function toggleRunning(task) {
     task.set("Running", !task.field("Running"));
 }
-function main() {
-    if (entry().field("Running")) {
-        finish(entry());
+function main(task) {
+    if (task.field("Running")) {
+        finish(task);
     } else {
-        start(entry());
+        start(task);
     }
+    toggleRunning(task);
+    var parent = get_parent(task);
+    if (parent) { main(parent); }
 }
 function elapsed() {
     return Date.now() - entry().field("Timer start");
@@ -51,49 +47,4 @@ function updateWaitTimes(duration) {
 }
 function create_log_entry(text) {
     libByName("Log").create({ Description: text, Datetime: Date.now() });
-}
-function max_wait_time(task) {
-    var max = 0;
-    var subtasks = task.field("Subtasks");
-    for (var i = 0; i < subtasks.length; i++) {
-        if (subtasks[i].field("Subtasks").length > 0) {
-            max = Math.max(max, max_wait_time(subtasks[i]));
-        } else {
-            max = Math.max(max, subtasks[i].field("Wait time"));
-        }
-    }
-    return Math.max(max, task.field("Wait time"));
-}
-function is_root(task) {
-    return get_parent(task) == undefined;
-}
-function start_timers(task) {
-    function start_timers_helper(task, duration) {
-        var subtasks = task.field("Subtasks");
-        if (duration > 0) {
-            if (!task.field("timer running")) {
-                setTimer(task, duration / 1000);
-                sleep(1);
-            }
-            if (subtasks.length > 0) {
-                start_timers_helper(
-                    running_child(task),
-                    time_slice(duration, to_array(subtasks), running_child(task))
-                );
-            }
-        }
-    }
-    start_timers_helper(task, max_wait_time(task));
-}
-function running_child(task) {
-    return to_array(task.field("Subtasks")).find(x => x.field("Running"));
-}
-function sleep(seconds) {
-    function nop() {
-        return;
-    }
-    var start = Date.now();
-    while (Date.now() < start + seconds * 1000) {
-        nop();
-    }
 }

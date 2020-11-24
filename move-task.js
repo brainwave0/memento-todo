0fc12e0e4dc33d906322270c227c945cc3839603
Ian Campos <iancampos.0@gmail.com>
2020-11-24 10:54:38 -0800

    fix

--- move-task.js
+++ move-task.js
@@ -7,8 +7,7 @@ function move_task(index) {
   } else if (index >= tasks.length) {
     new_vruntime = vruntime(tasks[index]) + 1;
   } else {
-    new_vruntime =
-      (vruntime(tasks[index - 1]) + vruntime(tasks[index + 1])) / 2;
+    new_vruntime = (vruntime(tasks[index - 1]) + vruntime(tasks[index])) / 2;
   }
 
   entry().set(

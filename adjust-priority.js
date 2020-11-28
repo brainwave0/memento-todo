64cb0fbfad5e25b168ac68f1d9a9fdefd615b637
Ian Campos <iancampos.0@gmail.com>
2020-11-28 10:41:51 -0800

    fix

--- adjust-priority.js
+++ adjust-priority.js
@@ -6,20 +6,29 @@ function adjust_priorities(amount) {
   var avg_priority =
     all_tasks.map((x) => x.field("Priority")).reduce((a, b) => a + b, 0) /
     all_tasks.length;
-  var selected_tasks = to_array(selectedEntries());
 
   // Increment the priorities of the selected tasks by the given amount,
   // or do the opposite by decrementing the priorities of the non-selected tasks,
   // so that there is a balance of positive and negative priorities.
-  if (avg_priority >= 0) {
-    selected_tasks.forEach((x) =>
-      x.set("Priority", x.field("Priority") - amount)
-    );
-  } else {
-    all_tasks.forEach((x) => {
-      if (!includes(selected_tasks, x)) {
-        x.set("Priority", x.field("Priority") + amount);
-      }
-    });
+  if (avg_priority < 0 && amount < 0) {
+    adjust_other(-amount);
+  } else if (avg_priority < 0 && amount > 0) {
+    adjust_selected(amount);
+  } else if (avg_priority > 0 && amount < 0) {
+    adjust_selected(amount);
+  } else if (avg_priority > 0 && amount > 0) {
+    adjust_other(-amount);
   }
+}
+function adjust_other(amount) {
+  to_array(selectedEntries()).forEach((x) =>
+    x.set("Priority", x.field("Priority") + amount)
+  );
+}
+function adjust_selected(amount) {
+  get_all_tasks().forEach((x) => {
+    if (!includes(to_array(selectedEntries()), x)) {
+      x.set("Priority", x.field("Priority") - amount);
+    }
+  });
 }

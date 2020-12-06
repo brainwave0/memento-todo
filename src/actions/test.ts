88e7760ccb0217978c6a35f04a9ea91165a7a875
Ian Campos <iancampos.0@gmail.com>
2020-12-06 15:12:15 -0800

    test

---
+++ src/actions/test.ts
@@ -0,0 +1,5 @@
+/// <reference path="../memento-database"/>
+/// <reference path="../util"/>
+/// <reference path="../sort-orders"/>
+
+message(remaining_runtime(active_tasks()));
--- src/memento-database.ts
+++ src/memento-database.ts
@@ -1,36 +1,39 @@
 class Entry {
-    fields: { name: any };
-    id: number;
-    field(name: string): any {
-        return this.fields[name];
-    }
-    set(name: string, value: any): void {
-        this.fields[name] = value;
-    }
+  fields: { name: any };
+  id: number;
+  field(name: string): any {
+    return this.fields[name];
+  }
+  set(name: string, value: any): void {
+    this.fields[name] = value;
+  }
 }
 class Entries {
-    length: number;
-    contents: Entry[];
+  length: number;
+  contents: Entry[];
 }
 class Library {
-    entries(): Entries {
-        return entries;
-    }
-    create(values) {
-        return;
-    }
+  entries(): Entries {
+    return entries;
+  }
+  create(values) {
+    return;
+  }
 }
 function entry(): Entry {
-    return current_entry;
+  return current_entry;
 }
 function lib(): Library {
-    return library;
+  return library;
 }
 function arg(name: string): any {
-    return args[name];
+  return args[name];
 }
 function libByName(name: string): Library {
-    return new Library();
+  return new Library();
+}
+function message(x): void {
+  return;
 }
 var current_entry: Entry;
 var library: Library;

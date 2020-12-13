23eb1bf4dc7dd2e541cf1d675522c6759f0d530c
Ian Campos <iancampos.0@gmail.com>
2020-12-13 13:01:57 -0800

    filter out tasks with null values when sorting

--- src/sort-orders.ts
+++ src/sort-orders.ts
@@ -1,23 +1,19 @@
 /// <reference path="./memento-database"/>
 var sort_orders = [
   function start_datetime(entries: Entry[]): Entry[] {
-    return sort(
-      entries,
-      (e) => e.field("Start datetime") || new Date(0),
-      SortDir.Ascending
-    );
+    return sort(entries, (e) => e.field("Start datetime"), SortDir.Ascending);
   },
   function deadline(entries: Entry[]): Entry[] {
     return sort(
-      entries,
-      (e) => e.field("Deadline") || new Date(0),
+      entries.filter((x) => x.field("Deadline")),
+      (e) => e.field("Deadline"),
       SortDir.Ascending
     );
   },
   function value(entries: Entry[]): Entry[] {
     return sort(
-      entries,
-      (e) => e.field("Value") / e.field("Total runtime") || 1,
+      entries.filter((x) => x.field("Total runtime")),
+      (e) => e.field("Value") / e.field("Total runtime"),
       SortDir.Descending
     );
   },

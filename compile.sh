9f81c025262b66d790b6cf79a7ca2e3b7eaf4fbe
Ian Campos <iancampos.0@gmail.com>
2020-12-17 23:38:47 -0800

    fix

--- compile.sh
+++ compile.sh
@@ -7,4 +7,7 @@ done
 babel --out-dir . --minified --no-comments out/*
 tsc --allowJs --module none --lib es2017,dom --outFile out/memento-database.ts.js src/memento-database.ts
 babel --minified --no-comments --out-file out/memento-database.js out/memento-database.ts.js
+for i in ./*.js; do
+    ./remove-contents.py out/memento-database.js $i
+done
 trash-put out

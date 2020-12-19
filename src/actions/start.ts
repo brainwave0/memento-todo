39951f431da00a1f23604b28ea0d43981e80e4b6
Ian Campos <iancampos.0@gmail.com>
2020-12-18 17:18:40 -0800

    fix

--- src/actions/start.ts
+++ src/actions/start.ts
@@ -1,6 +1,6 @@
 /// <reference path="../start-stop"/>
 function start(task): void {
-  assert(!entry().field("Running"), "task is already running");
+  assert(!task.field("Running"), "task is already running");
   create_log_entry('Started entry "' + task.field("Name") + '"');
   task.set("Timer start", new Date());
   task.set("Running", true);
